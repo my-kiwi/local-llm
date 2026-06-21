56e4aca974f56c37fbf19ccd63134b1c533686c1
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-06-21 13:19:52 +0000

    fixes: add types, use transformers fallback, improve error handling

--- src/chat.ts
+++ src/chat.ts
@@ -1,7 +1,27 @@
+/// <reference types="dom-chromium-ai" />
+
 // Set WEBLLM_CONFIG to select the WebLLM backend
-(window as any).WEBLLM_CONFIG = {
+// (window as any).WEBLLM_CONFIG = {
+//   apiKey: 'dummy', // Required for now by the loader
+//   modelName: 'Phi-2-q4f32_1-M', // Optional: override default
+// };
+
+(window as any).TRANSFORMERS_CONFIG = {
   apiKey: 'dummy', // Required for now by the loader
-  modelName: 'Phi-2-q4f32_1-M', // Optional: override default
+  device: 'webgpu', // 'webgpu' or 'cpu'
+  // dtype: 'q2f16', // Quantization level
+  // modelName: 'onnx-community/gemma-4-E2B-it-qat-mobile-ONNX',
+  modelName: 'onnx-community/Qwen2.5-0.5B-Instruct',
+  env: {
+    allowRemoteModels: true, // Allow loading models from remote URLs
+    backends: {
+      onnx: {
+        wasm: {
+          wasmPaths: 'https://cdn.example.com/wasm-assets/',
+        },
+      },
+    },
+  },
 };
 
 if (!('LanguageModel' in window)) {
@@ -9,16 +29,14 @@ if (!('LanguageModel' in window)) {
   await import('prompt-api-polyfill' as any);
 }
 
-const LanguageModel = (window as any).LanguageModel; // fixme any
-
 let llmAvailabilityStatus = 'unknown';
 let session: any = null; // fixme any
 
 // see https://developer.chrome.com/docs/ai/prompt-api?hl=fr#add_expected_input_and_output
-const llmOptions = {
-  expectedInputs: [{ type: 'text', languages: ['en' /* system prompt */, 'fr' /* user prompt */] }],
-  expectedOutputs: [{ type: 'text', languages: ['fr', 'en'] }],
-};
+
+const expectedInputs: LanguageModelExpected[] = [{ type: 'text', languages: ['en', 'fr'] }];
+
+const expectedOutputs: LanguageModelExpected[] = [{ type: 'text', languages: ['fr', 'en'] }];
 
 const getResponseOutput = () => {
   return document.getElementById('llm-response') as HTMLTextAreaElement;
@@ -55,45 +73,63 @@ export const Chat = (): string => {
 
 const loadLLM = async () => {
   llmAvailabilityStatus = 'checking';
-  let intervalId = setInterval(async () => {
-    llmAvailabilityStatus = await LanguageModel.availability(llmOptions);
-    getStatusElement().textContent = llmAvailabilityStatus;
-    if (llmAvailabilityStatus === 'available') {
-      clearInterval(intervalId);
-      session = await createSession();
-      console.log('LanguageModel session created:', session);
-      sendMessage(`Welcome me and present yourself in ${navigator.language} language.
+  getStatusElement().textContent = llmAvailabilityStatus;
+  llmAvailabilityStatus = await LanguageModel.availability({
+    expectedInputs,
+    expectedOutputs,
+  });
+  getStatusElement().textContent = llmAvailabilityStatus;
+  if (llmAvailabilityStatus === 'available') {
+    session = await createSession();
+    console.log('LanguageModel session created:', session);
+    sendMessage(`Welcome me and present yourself in ${navigator.language} language.
         Then ask me what I want to do. 
         Do not start with "allright" or "ok" or "sure" or "of course" or "certainly" or "absolutely" or "definitely" or "without a doubt".
         Keep it short but relaxed and friendly, add a smiley face at the end of your message.`);
-      getStatusElement().textContent = 'LanguageModel session created successfully.';
-      getResponseOutput().style.display = 'block';
-      getRequestInput().style.display = 'block';
-    }
-  }, 1000);
+    getStatusElement().textContent = 'LanguageModel session created successfully.';
+    getResponseOutput().style.display = 'block';
+    getRequestInput().style.display = 'block';
+  } else {
+    setTimeout(() => {
+      loadLLM();
+    }, 3000);
+  }
 };
 
 const createSession = async () => {
   getStatusElement().textContent = 'creating session...';
-  return await LanguageModel.create(llmOptions).catch((error: any) => {
+
+  try {
+    const session = await LanguageModel.create({
+      expectedInputs,
+      expectedOutputs,
+      monitor(monitor) {
+        monitor.addEventListener('downloadprogress', (e) => {
+          console.log(`Downloaded ${e.loaded * 100}%`);
+          getStatusElement().textContent = `Downloading model: ${Math.round(e.loaded * 100)}%`;
+        });
+      },
+      // initialPrompts:
+    });
+    console.log('LanguageModel session created:', session);
+    if (!session) {
+      throw new Error('no session created');
+    }
+    return session;
+  } catch (error: any) {
     console.error('Error creating LanguageModel session:', error);
-    getStatusElement().textContent = 'create error: ' + error.message;
-  });
+    getStatusElement().textContent = `create error: ${error.message} ${error.stack}`;
+    throw error;
+  }
 };
 
 function initializeChat() {
   const requestInput = getRequestInput();
-  const responseOutput = getResponseOutput();
-
   requestInput.addEventListener('keypress', async (event) => {
     if (event.key === 'Enter' && !event.shiftKey) {
       // TODO add send button for mobile
       console.log('Enter key pressed. User input:', requestInput.value);
       event.preventDefault();
-      if (!session) {
-        responseOutput.textContent = 'LanguageModel session not initialized yet.';
-        return;
-      }
       await sendMessage(requestInput.value.trim());
     }
   });
@@ -101,6 +137,10 @@ function initializeChat() {
 
 async function sendMessage(userInput: string) {
   const responseOutput = getResponseOutput();
+  if (!session) {
+    responseOutput.textContent = 'LanguageModel session not initialized yet.';
+    return;
+  }
   if (userInput) {
     responseOutput.textContent = 'Thinking...';
     try {
